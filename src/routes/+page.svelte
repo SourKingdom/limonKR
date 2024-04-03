e6b929cb58e8bbfb8978751971a3f1ab4ccf027f
Denatle <iq.labozin@gmail.com>
2024-04-04 00:11:14 +0300

    tried

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<!-- src/routes/+page.svelte -->
+<script lang="ts">
+	import { Auth } from '@supabase/auth-ui-svelte'
+	import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+	export let data
+
+	function click() {
+		console.log()
+	}
+</script>
+
+<svelte:head>
+	<title>User Management</title>
+</svelte:head>
+
+<div class="row flex-center flex">
+	<div class="col-6 form-widget">
+		<Auth
+			supabaseClient={data.supabase}
+			view="sign_in"
+			redirectTo="/account"
+			additionalData={[""]}
+			showLinks={true}
+			appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
+		/>
+	</div>
+</div>
