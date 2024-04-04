b19bf38ed8cd315df4f573de3dabf41aae5ce4d2
Denatle <iq.labozin@gmail.com>
2024-04-04 20:45:33 +0300

    minor fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,8 @@
 </script>
 
 {#if $page.data.session?.user != null}
-  <h1>Logged in</h1>
+  <h1>Logged in as {$page.data.session.user.name}</h1>
 {:else}
   <h1>Not logged in</h1>
 {/if}
+<a href="/login"><h1>Go to login page</h1></a>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,6 +4,7 @@
   import Button from "$lib/components/ui/Button.svelte";
 </script>
 
+<h1>Login page!</h1>
 {#if $page.data.session?.user}
   <p>Signed in as {$page.data.session.user.email}</p>
   <Button on:click={() => signOut()}>Sign out</Button>
@@ -11,3 +12,4 @@
   <p>Not signed in.</p>
   <Button on:click={() => signIn("google")}>Sign in</Button>
 {/if}
+
